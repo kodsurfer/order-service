f3d8f4ec8c6f81fbc892e5d3ce97d37b2064c45a
kodsurfer <kodsurfer@ya.ru>
2024-03-03 02:51:08 +0300

    add simple http server

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello world!");
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+        System.out.println("Server started on port 8000");
     }
 }
---
+++ src/main/java/org/example/MyHandler.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class MyHandler implements HttpHandler {
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String resp = "Hello World!";
+        exchange.sendResponseHeaders(200, resp.length());
+        OutputStream os = exchange.getResponseBody();
+        os.write(resp.getBytes());
+        os.close();
+    }
+}
