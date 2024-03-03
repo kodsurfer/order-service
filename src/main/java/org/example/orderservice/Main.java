ecb15751ca42d47bef6d3647b80e5bd231767ac2
kodsurfer <kodsurfer@ya.ru>
2024-03-04 01:50:06 +0300

    move files

--- src/main/java/org/example/orderservice/Main.java
+++ src/main/java/org/example/orderservice/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.orderservice;
 
 import com.sun.net.httpserver.HttpServer;
 
--- src/main/java/org/example/orderservice/MyHandler.java
+++ src/main/java/org/example/orderservice/MyHandler.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.orderservice;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
