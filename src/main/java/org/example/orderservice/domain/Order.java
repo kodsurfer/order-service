38370b1e2875b3625240805cdb4a70febce408e8
kodsurfer <kodsurfer@ya.ru>
2024-03-24 02:16:51 +0300

    format Order for readability

--- src/main/java/org/example/orderservice/domain/Order.java
+++ src/main/java/org/example/orderservice/domain/Order.java
@@ -3,15 +3,15 @@ package org.example.orderservice.domain;
 import java.util.Date;
 import java.util.Optional;
 
-record Order(long Id,
-             int GoodsCount,
-             double TotalSum,
-             double TotalWeight,
-             OrderType OrderType,
-             Date OrderDate,
-             Region Region,
-             OrderStatus OrderStatus,
-             String CustomerName,
-             Optional<Address> Address,
-             Optional<String> Phone) {
+public record Order(long Id,
+                    int GoodsCount,
+                    double TotalSum,
+                    double TotalWeight,
+                    OrderType OrderType,
+                    Date OrderDate,
+                    Region Region,
+                    OrderStatus OrderStatus,
+                    String CustomerName,
+                    Optional<Address> Address,
+                    Optional<String> Phone) {
 }
